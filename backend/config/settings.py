09e55e79e21db056eb7e3ebd7276524689edd7d7
syed-sadain <ssadain8682@gmail.com>
2026-04-27 22:45:17 +0530

    fix: env variable + frontend API connection

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -123,7 +123,7 @@ CORS_ALLOWED_ORIGINS = os.environ.get(
     "CORS_ALLOWED_ORIGINS",
     "http://localhost:3000,http://127.0.0.1:3000",
 ).split(",")
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_HEADERS = [
     "accept",
     "authorization",
