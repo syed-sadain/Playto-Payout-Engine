ae152ff4d2746d5e41d3144b7c8862199785f53d
syed-sadain <ssadain8682@gmail.com>
2026-04-27 20:21:55 +0530

    fix urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
 
+def home(request):
+    return JsonResponse({"message": "Payout Engine API is running 🚀"})
 
 def health_check(request):
     return JsonResponse({"status": "ok", "service": "playto-payout-engine"})
 
-
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("health/", health_check),
-    path("api/v1/", include("payouts.urls")),
+    path("", home),                      # ✅ root URL
+    path("admin/", admin.site.urls),     # ✅ admin panel
+    path("health/", health_check),       # ✅ health check
+    path("api/v1/", include("payouts.urls")),  # ✅ API routes
 ]
